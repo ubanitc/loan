96004c57362c54c6a7aeebef8b8ba448d25e6ba4
ubanitc <57474291+ubanitc@users.noreply.github.com>
2020-08-18 00:32:23 -0700

    Create callback.php

---
+++ callback.php
@@ -0,0 +1,21 @@
+<?php
+    $host = "us-cdbr-east-02.cleardb.com";
+$username = "bd698371ac76ff";
+$password = "2d522430";
+$dbname = "heroku_a04909d9b1187cf";
+
+$dsn = ("mysql:host=".$host.";dbname=".$dbname);
+
+
+   $dang = $pdo = new pdo($dsn,$username,$password);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    
+    if($dang){
+       echo "connection succesful";
+    }else{ 
+        echo "not connected";
+    }
+
+
+?>
